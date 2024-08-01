667783e67cbc3cb804f76afca82c9d33466ef51e
amit-negi-23 <amitnegiit23@gmail.com>
2024-08-01 19:53:38 +0530

    Example 1 : useState()

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
+import Learning_useState from "./1_useState/learningHook";
 import "./App.css";
 
 function App() {
 
-  let name = "JavaScript";
-  const changeLanguage = () => {
-    name = "React";
-    console.log(name);
-  };
-  
   return (
     <div className="container">
-      <h1>I am Learning {name}!</h1>
-      <button onClick={changeLanguage}>Add new </button>
+      <Learning_useState/>
     </div>
   );
 }
