66beff8981433d98c6b5fc4f45434859a76cfc1d
amit-negi-23 <amitnegiit23@gmail.com>
2024-08-05 13:15:13 +0530

    Example 2: useEffect()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Learning_useEffect from "./2_useEffect/learningHook";
+import Learning2_useEffect from "./2_useEffect/learningHook2";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
         <Learning_useEffect/>
+        <Learning2_useEffect/>
     </div>
   );
 }
