4c96e94308c20670f911c36aada4716889745fc6
amit-negi-23 <amitnegiit23@gmail.com>
2024-08-08 10:09:54 +0530

    Exaple 5: useRef() and forwardRef()

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Learning_useRef from "./3_useRef/learningHook";
 import Learning2_useRef from "./3_useRef/learningHook2";
 import Learning3_useRef from "./3_useRef/learningHook3";
 import Learning4_useRef from "./3_useRef/learningHook4";
+import Learning5_useRef from "./3_useRef/learningHook5";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
       <Learning3_useRef/>
       <br/><br/>
       <Learning4_useRef/>
+      <br/><br/>
+      <Learning5_useRef/>
     </div>
   );
 }
